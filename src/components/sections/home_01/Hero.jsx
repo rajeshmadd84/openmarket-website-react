7d9f15a083c17e5634b51192af6c380edfc8e79d
rajeshmadd84 <maddineni.rajesh@gmail.com>
2025-10-05 23:14:48 +0530

    om3

--- src/components/sections/home_01/Hero.jsx
+++ src/components/sections/home_01/Hero.jsx
@@ -9,44 +9,46 @@ const Hero = () => {
       <section id='section-hero'>
         <div className='relative z-[1] overflow-hidden rounded-bl-[30px] rounded-br-[30px] bg-colorLinenRuffle pb-20 pt-28 lg:rounded-bl-[50px] lg:rounded-br-[50px] lg:pb-24 lg:pt-32 xl:pt-40 xxl:pb-[133px] xxl:pt-[195px]'>
           <div className='global-container'>
-            <div className='mb-14 flex flex-col items-center text-center lg:mb-20'>
-              <h1 className='jos slide-from-bottom mb-6 max-w-[510px] lg:max-w-[768px] xl:max-w-[1076px]'>
-                Simplify your SaaS solution with AI
-              </h1>
-              <p className='jos slide-from-bottom mb-11 max-w-[700px] text-lg font-semibold sm:text-xl xl:max-w-[980px]'>
-                Our AI SAAS tool is a cloud-based software delivery model. It
-                helps businesses forecast demand for products and services and
-                optimize inventory management and supply chain operations.
-              </p>
+            <div className='grid grid-cols-1 items-center gap-10 lg:grid-cols-2 lg:gap-12'>
               <div
-                className='jos flex flex-wrap justify-center gap-6'
-                data-jos_animation='fade'
+                className='jos hero-img overflow-hidden rounded-2xl bg-black'
+                data-jos_animation='zoom'
               >
-                <Link
-                  to='/contact'
-                  className='button rounded-[50px] border-2 border-black bg-black py-4 text-white after:bg-colorOrangyRed hover:border-colorOrangyRed hover:text-white'
-                >
-                  Get started for free
-                </Link>
-                <Link
-                  to='/about'
-                  className='button rounded-[50px] border-2 border-black bg-transparent py-4 text-black after:bg-colorOrangyRed hover:border-colorOrangyRed hover:text-white'
-                >
-                  Learn more
-                </Link>
+                <img
+                  src='assets/img/th-1/hero-dashboard.jpg'
+                  alt='hero-dashboard'
+                  width='1296'
+                  height='640'
+                  className='h-auto w-full'
+                />
               </div>
-            </div>
-            <div
-              className='jos hero-img overflow-hidden rounded-2xl bg-black'
-              data-jos_animation='zoom'
-            >
-              <img
-                src='assets/img/th-1/hero-dashboard.jpg'
-                alt='hero-dashboard'
-                width='1296'
-                height='640'
-                className='h-auto w-full'
-              />
+              <div className='mb-14 flex flex-col items-center text-center lg:mb-20 lg:items-start lg:text-left'>
+                <h1 className='jos slide-from-bottom mb-6 max-w-[510px] lg:max-w-[768px] xl:max-w-[1076px]'>
+                  Simplify your SaaS solution with AI
+                </h1>
+                <p className='jos slide-from-bottom mb-11 max-w-[700px] text-lg font-semibold sm:text-xl xl:max-w-[980px]'>
+                  Our AI SAAS tool is a cloud-based software delivery model. It
+                  helps businesses forecast demand for products and services and
+                  optimize inventory management and supply chain operations.
+                </p>
+                <div
+                  className='jos flex flex-wrap justify-center gap-6 lg:justify-start'
+                  data-jos_animation='fade'
+                >
+                  <Link
+                    to='/contact'
+                    className='button rounded-[50px] border-2 border-black bg-black py-4 text-white after:bg-colorOrangyRed hover:border-colorOrangyRed hover:text-white'
+                  >
+                    Get started for free
+                  </Link>
+                  <Link
+                    to='/about'
+                    className='button rounded-[50px] border-2 border-black bg-transparent py-4 text-black after:bg-colorOrangyRed hover:border-colorOrangyRed hover:text-white'
+                  >
+                    Learn more
+                  </Link>
+                </div>
+              </div>
             </div>
 
             <div className='my-10 h-[1px] w-full bg-[#DBD6CF] lg:my-16 xl:my-20'></div>
